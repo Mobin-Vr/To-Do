163aa5cdc076631335d05f2f85dd844e011716c5
Mobin <Mobin.Vr@outlook.com>
2025-02-03 01:57:04 +0330

    Refactored: Improve some design icons

--- app/_components/MenuSidebar/CategoryItem.jsx
+++ app/_components/MenuSidebar/CategoryItem.jsx
@@ -1,15 +1,30 @@
-import MenuBtn from '../_ui/MenuBtn';
+import { ListIcon } from '@/public/icons';
 import SidebarLink from './SidebarLink';
+import useTaskStore from '@/app/taskStore';
+import { useShallow } from 'zustand/react/shallow';
 
 export default function CategoryItem({ category, toggleSidebar }) {
+   const { invitations } = useTaskStore(
+      useShallow((state) => ({
+         invitations: state.invitations,
+      }))
+   );
+
+   const hasCollab = invitations.includes(
+      (invitation) => invitation.invitation_category_id === category.category_id
+   );
+
    return (
       <SidebarLink
          href={`/tasks/${category.category_id}`}
          title={category.category_title}
          categoryId={category.category_id}
          onClick={toggleSidebar}
+         hasCollab={true}
       >
-         <MenuBtn className='text-[#3063ab]' />
+         <span className='text-blue-600'>
+            <ListIcon />
+         </span>
       </SidebarLink>
    );
 }
--- app/_components/MenuSidebar/SidebarLink.jsx
+++ app/_components/MenuSidebar/SidebarLink.jsx
@@ -5,8 +5,16 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import useTaskStore from '@/app/taskStore';
 import { defaultCategoryId } from '@/app/_lib/utils';
+import { UsersIcon } from '@/public/icons';
 
-const SidebarLink = ({ href, title, children, categoryId, onClick }) => {
+const SidebarLink = ({
+   href,
+   title,
+   children,
+   categoryId,
+   onClick,
+   hasCollab,
+}) => {
    const pathname = usePathname();
    const isActive = pathname === href;
 
@@ -51,10 +59,17 @@ const SidebarLink = ({ href, title, children, categoryId, onClick }) => {
                {title}
             </div>
 
-            <span className='flex items-center justify-center w-5 h-5 text-xs bg-blue-50 rounded-full text-gray-500 '>
-               {count()}
-            </span>
+            <div className='flex gap-3'>
+               {hasCollab && (
+                  <span className='text-gray-500 ml-1'>
+                     <UsersIcon size='18px' />
+                  </span>
+               )}
 
+               <span className='flex items-center justify-center w-5 h-5 text-xs bg-blue-50 rounded-full text-gray-500 '>
+                  <span> {count()}</span>
+               </span>
+            </div>
             {isActive && (
                <div className='absolute left-0 h-[50%] border-l-[3px] rounded-xl border-blue-500'></div>
             )}
