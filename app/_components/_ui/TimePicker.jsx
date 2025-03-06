6fe55e1129cdd8652c5c24de500bf1d6a99ac798
Mobin <Mobin.Vr@outlook.com>
2025-03-06 17:53:53 +0330

    Fix: Fixed some UI bugs

--- app/_components/_ui/TimePicker.jsx
+++ app/_components/_ui/TimePicker.jsx
@@ -6,17 +6,16 @@ export default function TimePicker({
 }) {
   return (
     <div className="mx-1 mb-2 flex w-60 items-center justify-start rounded-md bg-blue-700">
-      <span className="my-auto flex w-2/5 justify-center text-nowrap text-xs text-white">
+      <span className="my-auto flex w-1/2 justify-center text-nowrap text-xs text-white">
         Select Time
       </span>
-
       <input
         type="time"
         ref={timeInputRef}
         onClick={handleInputClick}
         onChange={(e) => setTime(e.target.value)}
         value={time}
-        className="focus:ring-none flex w-3/5 select-none justify-start rounded-md border-none bg-inherit p-1.5 pr-2 text-xl text-white outline-none focus:text-white focus:outline-none"
+        className="focus:ring-none mr-4 flex w-1/2 select-none justify-center rounded-md border-none bg-inherit p-1.5 text-xl text-white outline-none focus:text-white focus:outline-none"
       />
     </div>
   );
